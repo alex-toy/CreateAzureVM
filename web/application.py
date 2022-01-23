084606ebb187ede46665afcee6fd15ca97c18f0a
Alessio REA <area@dtek.fr>
2022-01-23 13:42:23 +0100

    init work-dev

--- web/application.py
+++ web/application.py
@@ -2,14 +2,13 @@
 This script runs the FlaskTemplate application using a development server.
 """
 
-
 from os import environ
 from FlaskTemplate import app
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '3000'))
+        PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
-        PORT = 3000
+        PORT = 5555
     app.run(HOST, PORT)
